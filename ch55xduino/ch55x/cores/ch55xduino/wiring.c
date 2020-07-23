65e695d9c666a952b02ad6bb35a49c545d0870a6
Deqing Sun <iamseer@gmail.com>
2020-07-23 14:23:30 -0400

    optimize micros

--- ch55xduino/ch55x/cores/ch55xduino/wiring.c
+++ ch55xduino/ch55x/cores/ch55xduino/wiring.c
@@ -10,7 +10,7 @@ extern __idata volatile uint32_t timer0_overflow_count;
 extern __idata volatile uint8_t timer0_overflow_count_5th_byte;
 
 uint32_t micros(){
-    volatile uint32_t m;
+    /*uint32_t m;
     uint8_t t;
     uint8_t interruptOn = EA;
     EA = 0;
@@ -28,7 +28,104 @@ uint32_t micros(){
     t=(t>>1)-3;
     m=m*125;
     
-    return ( m+t );
+    return ( m+t );*/
+  
+    //assembly has better support for multiplication
+    
+    __asm__ (";uint8_t interruptOn = EA; //to c            \n"
+             ";clr and mov won't affect c                  \n"
+             "    mov c,_EA                                \n"
+             ";EA = 0;                                     \n"
+             "    clr _EA                                  \n"
+             ";Copy _timer0_overflow_count to local R0~R3,m\n"
+             "    mov r0, (_timer0_overflow_count)         \n"
+             "    mov r1, (_timer0_overflow_count)+1       \n"
+             "    mov r2, (_timer0_overflow_count)+2       \n"
+             "    mov r3, (_timer0_overflow_count)+3       \n"
+             ";Copy TL0 to local R4, t                     \n"
+             "    mov r4, _TL0                             \n"
+             ";Copy TCON (TF0) to local R5                 \n"
+             "    mov b, _TCON                             \n"
+             ";if (interruptOn) EA = 1;                    \n"
+             "    mov _EA,c                                \n"
+             
+             ";if ((TF0 in b) && (R4 != 255)){              \n"
+             "    jnb b.5,incTimer0_overf_cntCopyOver$     \n"
+             "    mov a,#1     \n"
+             "    add a,r4     \n"
+             "    jz incTimer0_overf_cntCopyOver$\n"
+             
+             ";m++                                         \n"
+             "    inc r0                                   \n"
+             "    cjne r0,#0,incTimer0_overf_cntCopyOver$  \n"
+             "    inc r1                                   \n"
+             "    cjne r1,#0,incTimer0_overf_cntCopyOver$  \n"
+             "    inc r2                                   \n"
+             "    cjne r2,#0,incTimer0_overf_cntCopyOver$  \n"
+             "    inc r3                                   \n"
+             "incTimer0_overf_cntCopyOver$:                \n"
+             
+             ";1m = 250t 1t=0.5us (m*250+t-6)/2  t is 6~255\n"
+             ";we need to return m*125+t-3                 \n"
+             ";t=(t>>1)-3;                                 \n"
+             "    mov a,r4                                 \n"
+             "    clr c                                    \n"
+             "    rrc a                                    \n"
+             "    mov r4,a                                 \n"
+             "    dec r4                                   \n"
+             "    dec r4                                   \n"
+             "    dec r4                                   \n"
+             
+             ";m=m*125;                                    \n"
+             "    mov b, #125                              \n"
+             "    mov a, r0                                \n"
+             "    mul ab                                   \n"
+             "    mov r0, a                                \n"
+             "    mov r5, b                                \n"
+             
+             "    mov b, #125                              \n"
+             "    mov a, r1                                \n"
+             "    mul ab                                   \n"
+             "    add a, r5                                \n"
+             "    mov r1, a                                \n"
+             "    clr a                                    \n"
+             "    addc a, b                                \n"
+             "    mov r5, a                                \n"
+             
+             "    mov b, #125                              \n"
+             "    mov a, r2                                \n"
+             "    mul ab                                   \n"
+             "    add a, r5                                \n"
+             "    mov r2, a                                \n"
+             "    clr a                                    \n"
+             "    addc a, b                                \n"
+             "    mov r5, a                                \n"
+             
+             "    mov b, #125                              \n"
+             "    mov a, r3                                \n"
+             "    mul ab                                   \n"
+             "    add a, r5                                \n"
+             "    mov r3, a                                \n"
+         
+             ";return m+t                                  \n"
+             "    mov r5, #0                               \n"
+             "    mov a, r4                                \n"
+             "    add a, r0                                \n"
+             "    mov dpl, a                               \n"
+             "    mov a, r1                                \n"
+             "    addc a, r5                               \n"
+             "    mov dph, a                               \n"
+             "    mov a, r2                                \n"
+             "    addc a, r5                               \n"
+             "    mov b, a                                 \n"
+             "    mov a, r3                                \n"
+             "    addc a, r5                               \n"
+
+             );
+    
+    //return values: ’dpl’ 1B, ’dpl’ LSB & ’dph’ 2B,
+    //’dpl’, ’dph’ and ’b’ 3B, ’dpl’,’dph’,’b’ & ’acc’ 4B
+    
 }
 
 uint32_t millis()
@@ -36,6 +133,9 @@ uint32_t millis()
 
     // disable interrupts while we read timer0_millis or we might get an
     // inconsistent value (e.g. in the middle of a write to timer0_millis)
+    
+    //assembly has better support for 5 byte data type
+    //and efficient in shifting
 
     __asm__ (";uint8_t interruptOn = EA; //to c            \n"
              ";clr and mov won't affect c                  \n"
