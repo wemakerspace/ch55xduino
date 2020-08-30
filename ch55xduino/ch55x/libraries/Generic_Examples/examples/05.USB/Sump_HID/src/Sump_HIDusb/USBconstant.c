93952c5a52ccbc0a42dd610f1aef7ef7c54d2e68
Deqing Sun <iamseer@gmail.com>
2020-08-30 10:44:17 -0400

    update ID

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/Sump_HID/src/Sump_HIDusb/USBconstant.c
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/Sump_HID/src/Sump_HIDusb/USBconstant.c
@@ -7,7 +7,7 @@ __code uint8_t DevDesc[] = {
     0x10,0x01,  //USB spec release number in BCD format, USB1.1 (0x10, 0x01).
     0x00,0x00,0x00, //bDeviceClass, bDeviceSubClass, bDeviceProtocol
     DEFAULT_ENDP0_SIZE, //PACKET_SIZE
-    0x09,0x12,0x50,0xC5, // VID PID
+    0x86,0x1a,0x08,0xe0, // VID PID
     0x00,0x01,  //version
     0x01,0x02,0x03, //bString
     0x01    //bNumConfigurations
@@ -49,7 +49,7 @@ __code uint8_t CfgDesc[] ={
     0x05,                      /* bDescriptorType */
     0x01,         /* bEndpointAddress */
     0x03,                             /* bmAttributes */
-    0x40, 0x00,    /* wMaxPacketSize */
+    0x08, 0x00,    /* wMaxPacketSize */
     1,        /* bInterval */
     
     /* EP Descriptor: interrupt out. */
@@ -57,7 +57,7 @@ __code uint8_t CfgDesc[] ={
     0x05,                      /* bDescriptorType */
     0x81,       /* bEndpointAddress */
     0x03,                             /* bmAttributes */
-    0x40, 0x00,    /* wMaxPacketSize */
+    0x08, 0x00,    /* wMaxPacketSize */
     1         /* bInterval */
     
     
@@ -71,14 +71,14 @@ __code uint8_t ReportDesc[] ={
     0x09, 0x01,         // Usage (Vendor Usage 1)
     0xA1, 0x01,         // Collection (Application)
     0x19, 0x01,         // Usage Minimum
-    0x29, 0x40,         // Usage Maximum //64 input usages total (0x01 to 0x40)
+    0x29, 0x08,         // Usage Maximum //64 input usages total (0x01 to 0x40)
     0x15, 0x00,         // Logical Minimum (data bytes in the report may have minimum value = 0x00)
     0x26, 0xFF, 0x00,   // Logical Maximum (data bytes in the report may have maximum value = 0x00FF = unsigned 255)
     0x75, 0x08,         // Report Size: 8-bit field size
-    0x95, 0x40,         // Report Count: Make sixty-four 8-bit fields (the next time the parser hits an "Input", "Output", or "Feature" item)
+    0x95, 0x08,         // Report Count: Make sixty-four 8-bit fields (the next time the parser hits an "Input", "Output", or "Feature" item)
     0x81, 0x00,         // Input (Data, Array, Abs): Instantiates input packet fields based on the above report size, count, logical min/max, and usage.
     0x19, 0x01,         // Usage Minimum
-    0x29, 0x40,         // Usage Maximum //64 output usages total (0x01 to 0x40)
+    0x29, 0x08,         // Usage Maximum //64 output usages total (0x01 to 0x40)
     0x91, 0x00,         // Output (Data, Array, Abs): Instantiates output packet fields. Uses same report size and count as "Input" fields, since nothing new/different was specified to the parser since the "Input" item.
     0xC0                // End Collection
 };
@@ -94,10 +94,10 @@ __code uint8_t SerDes[]={                                 //Serial String Descri
 };
 __code uint16_t SerDesLen = sizeof(SerDes);
 __code uint8_t Prod_Des[]={                                //Produce String Descriptor
-    0x20,0x03,
+    0x1E,0x03,
     'C',0x00,'H',0x00,'5',0x00,'5',0x00,'x',0x00,' ',0x00,
-    'C',0x00,'M',0x00,'S',0x00,'I',0x00,'S',0x00,'-',0x00
-    ,'D',0x00,'A',0x00, 'P',0x00
+    'S',0x00,'U',0x00,'M',0x00,'P',0x00,' ',0x00,'H',0x00
+    ,'I',0x00,'D',0x00
 };
 __code uint16_t Prod_DesLen = sizeof(Prod_Des);
 
