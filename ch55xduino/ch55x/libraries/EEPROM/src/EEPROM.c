7cdcbda40faab2de4aaf0dad9bd0400129edb69f
Deqing Sun <iamseer@gmail.com>
2020-10-06 11:06:55 -0400

    add eeprom

---
+++ ch55xduino/ch55x/libraries/EEPROM/src/EEPROM.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 by Deqing Sun <ds@thinkcreate.us>
+ * Dataflash library for ch551/2.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+
+
+#include "EEPROM.h"
+
---
+++ ch55xduino/ch55x/libraries/EEPROM/src/EEPROM.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 by Deqing Sun <ds@thinkcreate.us>
+ * Dataflash library for ch551/2.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _EEPROM_H_INCLUDED
+#define _EEPROM_H_INCLUDED
+
+//#include <wiring_private.h>
+#include <Arduino.h>
+
+#endif
