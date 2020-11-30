a231ac8ee46dac38bf261a34a34eda8f360bc7fd
Deqing Sun <iamseer@gmail.com>
2020-11-30 18:29:05 -0500

    init work on lib

---
+++ ch55xduino/ch55x/libraries/Generic_Examples/WS2812/src/WS2812.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 by Deqing Sun <ds@thinkcreate.us> (c version for CH552 port)
+ * Touch key library for arduino CH552.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _WS2812_H_INCLUDED
+#define _WS2812_H_INCLUDED
+#include <Arduino.h>
+#include "template/WS2812_pins_header.h"
+
+#endif
