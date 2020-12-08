7025f95ab01f241307085787dbad801e52bcd48d
Deqing Sun <iamseer@gmail.com>
2020-12-07 20:22:19 -0500

    add mirror method

---
+++ mirror/addProxy.py
@@ -0,0 +1,14 @@
+#use
+#   https://gh-proxy.deqing.workers.dev/https://raw.githubusercontent.com/DeqingSun/ch55xduino/playground/mirror/package_ch55xduino_mcs51_index_proxy.json
+
+import urllib2  # the lib that handles the url stuff
+target_url = "https://raw.githubusercontent.com/DeqingSun/ch55xduino/ch55xduino/package_ch55xduino_mcs51_index.json"
+
+data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+
+
+out_file = open("package_ch55xduino_mcs51_index_proxy.json", "w")
+for line in data:
+    line = line.replace("https://github.com","https://gh-proxy.deqing.workers.dev/https://github.com")
+    n = out_file.write(line)
+out_file.close()
