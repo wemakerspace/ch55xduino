fdfdbb8a4372a191167471741e04fbb8936851e6
Deqing Sun <iamseer@gmail.com>
2020-12-07 20:30:47 -0500

    change file

--- mirror/addProxy.py
+++ mirror/addProxy.py
@@ -7,7 +7,7 @@ target_url = "https://raw.githubusercontent.com/DeqingSun/ch55xduino/ch55xduino/
 data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
 
 
-out_file = open("package_ch55xduino_mcs51_index_proxy.json", "w")
+out_file = open("package_ch55xduino_mcs51_proxy_index.json", "w")
 for line in data:
     line = line.replace("https://github.com","https://gh-proxy.deqing.workers.dev/github.com")
     n = out_file.write(line)
