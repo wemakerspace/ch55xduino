1dff8c02911859fe822ce5d517520cc20755a0fe
Deqing Sun <iamseer@gmail.com>
2020-12-14 21:43:34 -0500

    uint16_t to tidy usb string

--- ch55xduino/ch55x/cores/ch55xduino/USBconstant.h
+++ ch55xduino/ch55x/cores/ch55xduino/USBconstant.h
@@ -13,11 +13,10 @@
 extern __code uint8_t DevDesc[];
 extern __code uint8_t CfgDesc[];
 extern __code uint8_t LangDes[];
-extern __code uint8_t SerDes[];
-extern __code uint8_t Prod_Des[];
-extern __code uint8_t CDC_Des[];
-extern __code uint8_t WEBUSB_Des[];
-extern __code uint8_t Manuf_Des[];
+extern __code uint16_t SerDes[];
+extern __code uint16_t Prod_Des[];
+extern __code uint16_t CDC_Des[];
+extern __code uint16_t Manuf_Des[];
 
 
 
